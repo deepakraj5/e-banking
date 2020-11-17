bebbfeb87c48dedf13f8213346fe09f800f61974
deepakraj5 <deepakatr4@gmail.com>
2020-11-17 10:25:05 +0530

    small change

--- src/middleware/login.auth.js
+++ src/middleware/login.auth.js
@@ -8,11 +8,11 @@ router.post('/login', async (req, res) => {
     try {
         const user = await User.findOne({ email: req.body.email })
         if(!user) {
-            return res.status(400).send({ error: 'wrong credentails' })
+            return res.status(400).send({ error: 'wrong credentials' })
         }
         const verify = await bcrypt.compare(req.body.password, user.password)
         if(!verify) {
-            return res.status(400).send({ error: 'wrong credentails' })
+            return res.status(400).send({ error: 'wrong credentials' })
         }
 
         const token = await user.generateAuthToken()
